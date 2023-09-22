71566c7eb38b81293273ab487605486ff12d8bc7
Arilson Santos <arilsonsantos@gmail.com>
2023-09-22 17:42:48 -0300

    Validation token JWT

--- src/model/domain/UserTokenDomain.go
+++ src/model/domain/UserTokenDomain.go
@@ -5,6 +5,7 @@ import (
 	"github.com/arilsonsantos/crud-go.git/src/errors"
 	"github.com/golang-jwt/jwt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -35,4 +36,42 @@ func (ud *userDomain) GenerateToken() (string, *errors.ErrorDto) {
 
 	return tokenString, nil
 
+}
+
+func VerifyToken(tokenValue string) (UserDomainInterface, *errors.ErrorDto) {
+	secret := os.Getenv(JwtSecretKey)
+	token, err := jwt.Parse(RemoveBearerPrefix(tokenValue),
+		func(token *jwt.Token) (interface {
+		}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+				return []byte(secret), nil
+			}
+			return nil, errors.BadRequestError("Invalid token")
+		})
+
+	if err != nil {
+		//TODO Unauthorized
+		return nil, errors.BadRequestError("Invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		//TODO Unauthorized
+		return nil, errors.BadRequestError("Invalid token")
+	}
+
+	return &userDomain{
+		id:    claims["id"].(string),
+		email: claims["email"].(string),
+		name:  claims["name"].(string),
+		age:   int8(claims["age"].(float64)),
+	}, nil
+}
+
+func RemoveBearerPrefix(token string) string {
+	if strings.HasPrefix(token, "Bearer") {
+		token = strings.TrimPrefix(token, "Bearer")
+	}
+
+	return token
 }
